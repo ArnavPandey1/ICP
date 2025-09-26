f8f05ada2607c54620e41b6669ba346f28e162ee
Arnav Pandey <www.aakashpandey2004@gmail.com>
2025-09-26 19:31:23 +0530

    Add files via upload

---
+++ Assignment-1_ICP/Sliding Window Maximum/sliding-window-maximum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] arr, int k) {
+        int ans[] = new int[arr.length - k + 1];
+        Deque<Integer> dq = new LinkedList<>();
+        for (int i = 0; i < k - 1; i++) {
+            if (dq.isEmpty()) {
+                dq.addLast(i);
+            } else {
+                while (!dq.isEmpty() && arr[dq.peekLast()] < arr[i])
+                    dq.removeLast();
+                dq.addLast(i);
+            }
+        }
+        int l=0;
+        for (int i = k - 1; i < arr.length; i++) {
+            while (!dq.isEmpty() && arr[dq.peekLast()] < arr[i])
+                dq.removeLast();
+            dq.addLast(i);
+            if(dq.peekFirst()<=i-k){
+                dq.removeFirst();
+            }
+            ans[l++]=arr[dq.peekFirst()];
+        }
+        return ans;
+    }
+}
