e55d5862a9add9594aeb262f89a0cc6f1507c527
Arnav Pandey <www.aakashpandey2004@gmail.com>
2025-10-08 01:00:11 +0530

    Add files via upload

---
+++ Assignment-9_ICP/reorder-list/reorder-list.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+       List<ListNode>l1=new ArrayList<>();
+       ListNode temp=head;
+       while(temp!=null){
+        l1.add(temp);
+        temp=temp.next;
+       } 
+       int n=l1.size();
+       for(int i=0;i<l1.size()/2;i++){
+           l1.get(i).next=l1.get(n-1-i);
+           l1.get(n-1-i).next=l1.get(i+1);
+       }
+       l1.get(n/2).next=null;
+    }
+}
