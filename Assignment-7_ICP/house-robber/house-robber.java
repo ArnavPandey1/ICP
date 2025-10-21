b2a20481cae16c65f2e100d5dcad086de23ec9cb
Arnav Pandey <www.aakashpandey2004@gmail.com>
2025-10-21 23:30:50 +0530

    Add files via upload

---
+++ Assignment-7_ICP/house-robber/house-robber.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return Robber(nums, 0, dp);
+    }
+
+    public static int Robber(int[] arr, int i, int[] dp) {
+        if (i >= arr.length) {
+            return 0;
+        }
+
+        if (dp[i] != -1) {
+            return dp[i];   
+        }
+
+        int rob = arr[i] + Robber(arr, i + 2, dp);
+        int dontRob = Robber(arr, i + 1, dp);
+        dp[i] = Math.max(rob, dontRob);
+        return dp[i];
+    }
+}
+/*
+Time Complexity: O(n)
+
+Space Complexity: O(n) (DP + recursion stack)
+*/
