fbb70a1722934bb15948945291c414ec8e8bd484
Arnav Pandey <www.aakashpandey2004@gmail.com>
2026-02-02 14:25:04 +0530

    Time: 114 ms (5.14%) | Memory: 49.2 MB (5.67%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -10,31 +10,31 @@
  */
 class Solution {
     public ListNode doubleIt(ListNode head) {
-        String str="";
+        String str = "";
         ListNode temp = head;
         while (temp != null) {
-            str=str+temp.val;
+            str = str + temp.val;
             temp = temp.next;
         }
-        StringBuilder sb=new StringBuilder();
-        int t=0;
-        for(int i=str.length()-1;i>=0;i--){
-            char ch=str.charAt(i);
-            int s=t+ch-'0'+ch-'0';
-            t=s/10;
-            sb.append(s%10);
+        StringBuilder sb = new StringBuilder();
+        int t = 0;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char ch = str.charAt(i);
+            int s = t + ch - '0' + ch - '0';
+            t = s / 10;
+            sb.append(s % 10);
         }
-        if(t!=0){
-             sb.append(t);
+        if (t != 0) {
+            sb.append(t);
         }
         sb.reverse();
-        ListNode dummy=new ListNode();
-        ListNode dummyHead=dummy;
-        for(int i=0;i<sb.length();i++){
-            char ch=sb.charAt(i);
-            ListNode tt=new ListNode(ch-'0');
-            dummy.next=tt;
-            dummy=dummy.next;
+        ListNode dummy = new ListNode();
+        ListNode dummyHead = dummy;
+        for (int i = 0; i < sb.length(); i++) {
+            char ch = sb.charAt(i);
+            ListNode tt = new ListNode(ch - '0');
+            dummy.next = tt;
+            dummy = dummy.next;
         }
         return dummyHead.next;
     }
